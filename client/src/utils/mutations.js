50ae2d2e85031653c3736af63c579b474f13a71c
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-03-27 11:44:29 -0500

    mutation queries for login_user, add_user, save_book, and remove_book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,65 @@
-// LOGIN_USER executes loginUser 
+import { gql } from '@apollo/client';
 
+
+// LOGIN_USER executes loginUser 
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
 
 
 // ADD_USER executes addUser mutation
-
-
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
 
 // SAVE_BOOK executes saveBook mutation
-
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: bookInput!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                title
+                image
+                description
+                authors
+                link
+            }
+        }
+    }
+`;
 
 
 //REMOVE_BOOK  executes remove book mutation
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            savedBooks {
+                bookId
+                title
+                authors
+                link
+            }
+        }
+    }
+`;
